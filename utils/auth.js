54694d35c3ec2909bd8c2b5a80e63cb5218a0077
Christina R <xsnachannell@gmail.com>
2022-05-30 21:29:49 -0700

    updates auth.js with authenticated middleware function that checks to ensure user is logged in, before passing them through to the intended route

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,10 @@
+//middleware used to check whether user is logged in. If so, the "next" parameter allows the application to move forward to the route that the user was trying to access.
 const authenticated = (req, res, next) => {
-    if ()
-}
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = authenticated;
