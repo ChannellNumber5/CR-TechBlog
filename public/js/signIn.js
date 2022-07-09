cd2ed061644edd50e5b0d4f5fc3b6f85d0536a71
Christina R <xsnachannell@gmail.com>
2022-07-09 13:13:17 -0700

    updates signIn.js and signUp handlebars with function to handle front end signup form and addes email field to sign up handlebars

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -32,28 +32,28 @@ async function signUpHandler(event) {
     event.preventDefault();
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
-    if (!username) {
-        alert("Please Enter a username");
-        document.getElementById('logInForm').reset();
-    }
+    const email = document.querySelector('#userEmail').value;
+    const verifPass = document.querySelector('#repass').value;
 
-    if (!password) {
-        alert("Please Enter a username");
-        document.getElementById('logInForm').reset();
-    }
+    if (username && email && (password === verifPass)) {
+        const response = await fetch('api/user', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: {'Content-Type':'application/json' },
+        });
 
-    const response = await fetch('api/user', {
-        method: 'POST',
-        body: JSON.stringify({
-            username,
-            password
-        }),
-        headers: {'Content-Type':'application/json' },
-    });
-
-    if (response.ok) {
-        document.location.replace('/dashboard')
+        if (response.ok) {
+            document.location.replace('/dashboard')
+        } else {
+            alert(response.statusText);
+        }
+    } else if (password !== verifPass) {
+        alert("Please ensure password and re-entered password match");
     } else {
-        alert(response.statusText);
+        alert("Please ensure all required fields are complete");
     }
 };
