f2cfd2d3a00e5b1f81ff92a4b4fb8ccef25f07ec
Christina R <xsnachannell@gmail.com>
2022-07-29 13:26:13 -0700

    updates homeRoutes.js with route to render homePage and user dashboard with their posts. Also updates handlebars views for dashboard, homepage and postpage so that it pulls in the plainPosts array and not just posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,40 @@
 const { router } = require("express").Router();
 const { User, Post, Comment } = require('../models');
+const Authenticated = require('../utils/auth');
 
 //homePage shows all Posts posted on the blog from all users
-router.get('/homePage');
+router.get('/homePage', Authenticated, async (req, res) => {
+    try {
+        const posts = await Post.findAll();
+        if(!posts) {
+            res.json({message: "No Posts Found."})
+            .render('homePage');
+        }
+
+        const plainPosts = posts.map((posts) => posts.get({ plain:true }));
+        res.render('homePage', { plainPosts});
+    } catch (err) {
+        res.status(500).json({message: 'Error loading posts'})
+        .render('homePage');
+    }
+});
 
 //dashboard shows user's page and their specific posts
-router.get('/dashboard');
+router.get('/dashboard', Authenticated, async (req, res) => {
+    try {
+        const posts = await Post.findAll({ where: {userId: req.session.userId}});
+        if(!posts) {
+            res.json ({message: "No Posts Found."})
+            .render('dashboard');
+        }
+
+        const plainPosts = posts.map((posts) => posts.get({ plain:true }));
+        res.render('dashboard', { plainPosts});
+    } catch (err) {
+        res.status(500).json({message: 'Error loading posts'})
+        .render('dashboard');
+    }
+});
 
 //postPage shows the specific page for each post and associated comments with post. Users can also add comments to posts from this page
 router.get('/postPage');
