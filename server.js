7861e4b548731945600bb9993313fc5e18c0dd88
Christina R <xsnachannell@gmail.com>
2022-08-03 23:17:10 -0700

    updates server.js file to delete unnecessary spaces and import in auth.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session'); //allows for application to use cookies to track user sessions and will create a session table in your db
 const exphbars = require('express-handlebars'); //needed to use handlebards for our view in the MCV application development concept
 const routes = require('./controllers'); //connects to controllers index.js, to follow the controllers part of the MCV development concept
-
+const Auth = require('./utils/auth');
 
 const sequelize = require('./config/connection'); //establishes the connection to the database through the connection.js
 
@@ -12,7 +12,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store) // cr
 const app = express(); //creates a new instance of the express application
 const PORT = process.env.PORT || 3001;
 
-const hbars = exphbars.create(); // according to the documentation, this creates a new environment for the handlebars framework to exist in, but we have no helpers to export in
+const hbars = exphbars.create({Auth}); // according to the documentation, this creates a new environment for the handlebars framework to exist in, but we have no helpers to export in
 
 //creates new express session object, where the secret key is defined in the environment variables, the cookie defaults are overridden, the cookies do not resave, but any session is saved automatically, and any session inforamtion is stored in the new sequelize database created
 const sesh = {
@@ -32,16 +32,16 @@ app.engine('handlebars', hbars.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname, 'public'))); // serves out public static files when the initial url path is routed to
+app.use(express.urlencoded({ extended:true }));
+app.use(express.static(path.join(__dirname, 'public'))); // serves out public static files
 
 app.use(routes); //uses the controller to handle routes and requests to routes
 
 //syncs all sequelize models in the database, without overwriting them, and then starts up the server
-sequelize.sync({force:false}).then(() => {
+sequelize.sync({ force:false }).then(() => {
     app.listen(PORT, 'localhost', () => {
         console.log(`The server is listening at Port: ${PORT}`);
-    })
+    });
 });
 
 
