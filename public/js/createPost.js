03d32d30d12616b58df27070a2b30ffff7deb72f
Christina R <xsnachannell@gmail.com>
2022-08-29 20:16:50 -0700

    updates createPost.js so user can create a post and have it appear on their dashboard

--- public/js/createPost.js
+++ public/js/createPost.js
@@ -1,19 +1,18 @@
 
 async function createPostHandler (event) {
-    const title = document.querySelector('#title');
-    const content = document.querySelector('postText');
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('postText').value;
 
     event.preventDefault();
     
-    
-        const response = await fetch(`api/posts/`, {
-            method: 'POST',
-            body: JSON.stringify({
-                title,
-                content
-            }),
-            headers: {'Content-Type' : 'application/json'},
-        });
+    const response = await fetch(`/api/posts/`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: {'Content-Type' : 'application/json'},
+    });
 
         if (response.ok) {
             window.location.replace('/dashboard');
